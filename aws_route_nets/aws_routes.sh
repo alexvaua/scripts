31f9d6056ad1ec86b9bf06989787cb3eda9039d2
Oleksandr Medvid <alex.medve@gmail.com>
2021-10-18 20:21:42 +0300

    amore fixes related to Linux

--- aws_route_nets/aws_routes.sh
+++ aws_route_nets/aws_routes.sh
@@ -22,7 +22,7 @@ function manage_routes() {
     do
         sudo /sbin/route "$ACTION" -net "$line" "$pref" "$GW_IP"
     done
-    echo "Added routes$(wc -l "$RANGES"|cut -d/ -f-1)" |adate| tee -a "$LOG"
+    echo "$ACTION routes $(wc -l "$RANGES"|cut -d/ -f-1)" |adate| tee -a "$LOG"
 }
 
 function change_routes() {
@@ -53,7 +53,7 @@ function main() {
     else
         EXIST_ROUTE=$(netstat -nr4|awk '{print $1}'|grep -q "$(head -n1 "$RANGES" \
         |awk -F":" '{gsub(/"/, "", $2); print $2}'|cut -d"," -f1|cut -d" " -f2-)"; echo $?)
-        if [ $(( $(date +%s) - $(stat -f %m "$RANGES") )) -gt 43200 ]; then
+        if [ $(( $(date +%s) - $( if [ "$OS" == "Linux" ]; then date +%s -r "$RANGES"; else stat -f %m "$RANGES"; fi) )) -gt 43200 ]; then
             echo "$RANGES was no modified in last 12 Hours(43200s)" |adate| tee -a "$LOG"
             get_ranges
             change_routes
