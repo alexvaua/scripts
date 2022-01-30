234d82b9f36ebbb1cc43e3a05bf8e417513fb651
mixua <oleksandr.medvid@vgs.io>
2022-01-30 21:03:16 +0200

    optimized a bit 	modified:   ping_targets/dst_ping.sh

--- ping_targets/dst_ping.sh
+++ ping_targets/dst_ping.sh
@@ -8,18 +8,14 @@ TARGETS="AntonHomeSW HomeStolbSW GaponStolbRT MaslukStolbRT TulnovaHomeRT"
 
 function push_slack() {
         if [[ -n "$results" ]]; then
-                STATUS=danger
-		echo "$results" |grep -E  "failure" > /dev/null && STATUS=warning
+                STATUS=warning
+		echo "$results" |grep -E  "failure|denied" > /dev/null && STATUS=danger
                 echo "$results" |/usr/local/bin/slacktee.sh -u "$HOST" -t "$APP" -a $STATUS -e "cron date" "$(date)"
         fi
 }
 
 for i in $TARGETS; do
-<<<<<<< HEAD
-	results=$(ping_result=$(ping -q -W3 -c2 "$i" | grep -E "failure|timeout|Unreachable|100% packet loss")
-=======
 	results=$(ping_result=$(ping -q -W3 -c2 "$i" | grep -E "failure|timeout|Unreachable|denied|100% packet loss")
->>>>>>> 84de351... chore
 	[[ -z "$ping_result" ]] && echo > /dev/null || echo "dst $i $ping_result")
 	push_slack;
 done
