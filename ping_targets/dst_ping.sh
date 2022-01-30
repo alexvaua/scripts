0440d39759487c2e19af6d865b435c3407f196f1
mixua <oleksandr.medvid@vgs.io>
2022-01-30 18:34:39 +0200

    adjusted dst_ping script a bit

--- ping_targets/dst_ping.sh
+++ ping_targets/dst_ping.sh
@@ -1,21 +1,20 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+STATUS=good
 APP="check_icmp dst hosts"
 HOST=$(cat /etc/hostname).mixua.net
 TARGETS="AntonHomeSW HomeStolbSW GaponStolbRT MaslukStolbRT TulnovaHomeRT"
 
 function push_slack() {
-        if [[ ! -z "$results" ]]; then
-                echo $results |grep -e "success" > /dev/null && status=good || status="#858feb"
-                echo $results |grep -e  "timeout|Unreachable|denied" > /dev/null && status=warning || status=$status
-                echo $results |/usr/local/bin/slacktee.sh -u "$HOST" -t "$APP" -a $status -e "cron date" "$(date)"
+        if [[ -n "$results" ]]; then
+                echo "$results" |grep -E  "timeout|Unreachable|denied" > /dev/null && STATUS=warning
+                echo "$results" |/usr/local/bin/slacktee.sh -u "$HOST" -t "$APP" -a $STATUS -e "cron date" "$(date)"
         fi
 }
 
-# AntonHomeSW
 for i in $TARGETS; do
-	results=$(ping_result=$(ping -w3 -c2 $i | egrep -e "timeout|Unreachable|100% packet loss")
+	results=$(ping_result=$(ping -q -W3 -c2 "$i" | grep -E "timeout|Unreachable|100% packet loss")
 	[[ -z "$ping_result" ]] && echo > /dev/null || echo "dst $i $ping_result")
 	push_slack;
 done
