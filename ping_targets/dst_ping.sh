6e07df100f3ef4a9075c71d2aa28f8aabcb8c248
Oleksandr Medvid <alex.medve@gmail.com>
2022-02-15 13:49:22 +0200

    stopped flooding the slack channel

--- ping_targets/dst_ping.sh
+++ ping_targets/dst_ping.sh
@@ -7,10 +7,15 @@ HOST=$(cat /etc/hostname).mixua.net
 TARGETS="AntonHomeSW HomeStolbSW GaponStolbRT MaslukStolbRT TulnovaHomeRT"
 
 function push_slack() {
-        if [[ -n "$results" ]]; then
+        if [[ -n "$results" ]] && [[ ! -f tmp.txt ]] ; then
                 STATUS=warning
+                echo $STATUS > tmp.txt
 		echo "$results" |grep -E  "failure|denied" > /dev/null && STATUS=danger
                 echo "$results" |/usr/local/bin/slacktee.sh -u "$HOST" -t "$APP" -a $STATUS -e "cron date" "$(date)"
+        else    
+                echo "Connection restored $results" | \
+                /usr/local/bin/slacktee.sh -u "$HOST" -t "$APP" -a good -e "cron date" "$(date)"
+                rm tmp.txt
         fi
 }
 
