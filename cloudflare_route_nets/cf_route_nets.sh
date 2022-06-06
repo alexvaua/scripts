895f4b1b44edb1e5ec8a9c34e189eac8bd647585
Oleksandr Medvid <alex.medve@gmail.com>
2022-06-06 15:33:40 +0300

    added script that add cloudflare rule to ipfw

---
+++ cloudflare_route_nets/cf_route_nets.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+RULE_N=1399
+EXT_IPS="192.168.1.1"
+CF_NETS=$(curl -s https://www.cloudflare.com/ips-v4|awk '{print $0","};BEGIN {ORS=""}'|rev|cut -c 2-|rev)
+CHECK=$(ipfw show ${RULE_N} >/dev/null && echo $? || echo $?)
+
+if [[ ${CHECK} -ne 0 ]]; then
+	ipfw add ${RULE_N} allow tcp from ${CF_NETS} to ${EXT_IPS} dst-port 80,443,1723 in via re0 setup
+fi
