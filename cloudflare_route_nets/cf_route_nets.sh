2b0b08e1e51920af3200d82da11898382c92b315
Oleksandr Medvid <alex.medve@gmail.com>
2023-03-13 14:49:17 +0200

    minor changes

--- cloudflare_route_nets/cf_route_nets.sh
+++ cloudflare_route_nets/cf_route_nets.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 RULE_N=1399
-EXT_IPS="${EXT_IPS:=192.168.1.1}"
-CF_NETS=$(curl -s https://www.cloudflare.com/ips-v4|awk '{print $0","};BEGIN {ORS=""}'|rev|cut -c 2-|rev)
+EXT_IPS="${EXT_IPS:=192.168.33.1}"
+CF_NETS=$(curl -s https://www.cloudflare.com/ips-v4 | awk '{print $0","};BEGIN {ORS=""}' | rev | cut -c 2- | rev)
 CHECK=$(ipfw show ${RULE_N} >/dev/null && echo $? || echo $?)
 
 if [[ ${CHECK} -ne 0 ]]; then
